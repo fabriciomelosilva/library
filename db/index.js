23eeb53215476f6cf8cb3fa40ccac1a84b54fdc8
Fabrício Melo Silva <fabricio.melo@svm.com.br>
2025-08-07 17:07:29 -0300

    add db

--- db/index.js
+++ db/index.js
@@ -2,11 +2,10 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
+  connectionString: process.env.PG_CONNECTION_STRING,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
