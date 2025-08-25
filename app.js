ed8f473b84402b00dc9d55d0f9a8f5c2c142fc6f
davi.cardoso <davi.cardoso@svm.com.br>
2025-08-25 08:35:02 -0300

    CORS

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bookRoutes = require('./routes/books');
 const loanRoutes = require('./routes/loans');
-
+ 
+const cors = require('cors');
+ 
 const app = express();
+ 
+app.use(cors());
 const PORT = 3000;
-
+ 
 app.use(bodyParser.json());
 app.use('/books', bookRoutes);
 app.use('/loans', loanRoutes);
-
+ 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
+ 
